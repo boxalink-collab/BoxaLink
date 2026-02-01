3523d3fc891af760014bb98f2f4d1f1407b1c630
boxalink-collab <boxalink@gmail.com>
2026-02-01 12:05:10 +0000

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/
 import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
 
 // Importation des modules (Ils seront remplis après)
-import { initAuth } from './modules/auth.js';
-import { initChats } from './modules/chats.js';
-import { initAgora } from './modules/agora.js';
-import { initProfile } from './modules/profile.js';
+import { initAuth } from '/BoxaLink/modules/auth.js';
+import { initChats } from '/BoxaLink/modules/chats.js';
+import { initAgora } from '/BoxaLink/modules/agora.js';
+import { initProfile } from '/BoxaLink/modules/profile.js';
 // Ajoute ici les autres imports : forums, library, market
 
 const firebaseConfig = {
